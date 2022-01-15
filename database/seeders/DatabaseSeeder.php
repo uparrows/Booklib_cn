09a40557261b03001f80ec16bdd139cb0b85d209
unknown <dezhaoma@gmail.com>
2022-01-15 22:33:38 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $this->call(setting_thumbnail::class);
+        $this->call(scanning_frequency::class);
+
+    }
+}
